895335819a53b6558dda3c05869125cd615e8092
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2025-04-15 13:15:05 -0300

    finish snapshot to correct building and only fetching data if is not empty

--- index.ts
+++ index.ts
@@ -4,54 +4,65 @@ import { getUsers } from "./src/controllers/UserController.js";
 import { Clock } from "./src/models/interfaces/Clock.js";
 import { NSR } from "./src/models/interfaces/NSR.js";
 import { AFD } from "./src/models/interfaces/AFD.js";
-import * as util from 'util';
+import * as util from "util";
 import { RawCSV } from "./src/models/interfaces/RawCSV.js";
-import { getCpfFromCsvLine, parseCsv, parseDuplicatedData } from "./src/utils/Parsers.js";
-import { getAfdByInitialNSR, getAllClocks, getLastNSR, RecordLastNSR, WriteClockUsers } from "./src/controllers/ClockController.js";
+import {
+  getCpfFromCsvLine,
+  parseCsv,
+  parseDuplicatedData,
+} from "./src/utils/Parsers.js";
+import {
+  getAfdByInitialNSR,
+  getAllClocks,
+  getLastNSR,
+  RecordLastNSR,
+  WriteClockUsers,
+} from "./src/controllers/ClockController.js";
 import { MergeUsers } from "./src/utils/Merger.js";
 
 dotenv.config();
 
 //GET ALL TIME CLOCKS
-const clocks = await getAllClocks()
+const clocks = await getAllClocks();
 //console.log(clocks)
 
 // GET ALL NSRS
-const last_nsrs = await getLastNSR()
+const last_nsrs = await getLastNSR();
 //console.log(last_nsrs)
 
+const afds: AFD[] = [];
+const raw_csvs: RawCSV[] = [];
 
-const afds: AFD[] = []
-const raw_csvs: RawCSV[] = []
-
-const users_from_csv = new Map<number, string>()
-let headers: string | undefined = ''
+const users_from_csv = new Map<number, string>();
+let headers: string | undefined = "";
 
 await Promise.all(
-
   clocks.map(async (clock: Clock) => {
-
     // login to clock
     const session = await login(clock);
 
-    if (session) { //if valid sessiojn
+    if (session) {
+      //if valid sessiojn
       const last_nsr = last_nsrs.find(
         (last_nsr: NSR) => last_nsr.clock_id === clock.id
       );
 
-      const afd = await getAfdByInitialNSR(session, clock, Number(last_nsr.last_nsr) + 2) //get afd by intial nsr
+      const afd = await getAfdByInitialNSR(
+        session,
+        clock,
+        Number(last_nsr.last_nsr) + 2
+      ); //get afd by intial nsr
 
       //console.log(parseCsv(raw_csv))
 
       await logout(clock, session);
 
       const afd_object = {
-        "clock_id": clock.id,
-        "afd": afd
-      } as AFD
+        clock_id: clock.id,
+        afd: afd,
+      } as AFD;
 
-      afds.push(afd_object)
-      
+      afds.push(afd_object);
     } else {
       console.log("Failed login");
       return [];
@@ -59,66 +70,39 @@ await Promise.all(
   })
 );
 
-
-const parsedDuplicated = parseDuplicatedData(afds)
+const parsedDuplicated = parseDuplicatedData(afds);
 //console.log(`length: ${Array.from(parsedDuplicated.values()).length}`)
-console.log(parsedDuplicated)
+console.log(parsedDuplicated);
 
 await Promise.all(
   clocks.map(async (clock: Clock) => {
-
     // login to clock
     const session = await login(clock);
 
-    if (session) { //if valid sessiojn
-      
-      const users_csv = await getUsers(clock, session) // export all users in csv format
+    if (session) {
+      //if valid sessiojn
+
+      const users_csv = await getUsers(clock, session); // export all users in csv format
       //console.log(users_csv)
-      const parsedCSV = parseCsv(users_csv) // array of strings
-      headers = parsedCSV.header
+      const parsedCSV = parseCsv(users_csv); // array of strings
+      headers = parsedCSV.header;
 
-      for(const line of parsedCSV.lines)
-      {
-        const cpf = getCpfFromCsvLine(line)
+      for (const line of parsedCSV.lines) {
+        const cpf = getCpfFromCsvLine(line);
 
-        if(!cpf || !parsedDuplicated.has(cpf)) continue
+        if (!cpf || !parsedDuplicated.has(cpf)) continue;
 
-        const existingEntry = users_from_csv.get(cpf)
+        const existingEntry = users_from_csv.get(cpf);
 
-        if(!existingEntry || parsedDuplicated.get(cpf)?.clock_id === clock.id)
-        {
-          users_from_csv.set(cpf, line)
+        if (
+          !existingEntry ||
+          parsedDuplicated.get(cpf)?.clock_id === clock.id
+        ) {
+          users_from_csv.set(cpf, line);
         }
       }
 
       await logout(clock, session);
-
-      
-    } else {
-      console.log("Failed login");
-      return [];
-    }
-  })
-)
-
-//console.log(`length: ${Array.from(users_from_csv.values()).length}`)
-const new_csv = `${headers}\r\n${MergeUsers(users_from_csv)}`
-
-console.log(new_csv)
-
-let sucess: string | boolean = false
-await Promise.all(
-
-  clocks.map(async (clock: Clock) => {
-
-    // login to clock
-    const session = await login(clock);
-
-    if (session) { //if valid sessiojn
-      
-      sucess = await WriteClockUsers(new_csv, clock, session)
-      await logout(clock, session)
-      
     } else {
       console.log("Failed login");
       return [];
@@ -126,4 +110,36 @@ await Promise.all(
   })
 );
 
-if(sucess) RecordLastNSR(parsedDuplicated)
+//console.log(`length: ${Array.from(users_from_csv.values()).length}`)
+const mergegedUsers = MergeUsers(users_from_csv);
+
+if (mergegedUsers) {
+  const new_csv = `${headers}\r\n${mergegedUsers}`;
+
+  console.log(new_csv);
+
+  let sucess: string | boolean = false;
+
+  await Promise.all(
+    clocks.map(async (clock: Clock) => {
+      // login to clock
+      const session = await login(clock);
+
+      if (session) {
+        //if valid sessiojn
+
+        sucess = await WriteClockUsers(new_csv, clock, session);
+        await logout(clock, session);
+      } else {
+        console.log("Failed login");
+        return [];
+      }
+    })
+  );
+
+  if (sucess) RecordLastNSR(parsedDuplicated);
+}
+else
+{
+  console.log("Nenhum registro encontrado")
+}
